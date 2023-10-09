b65d4b464c7b8f4168558c70f62eab9c4b93a0f5
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2023-10-09 12:13:25 +0300

    webhook test

--- bot.py
+++ bot.py
@@ -398,8 +398,8 @@ if __name__ == '__main__':
     setup_application(app, dp, bot=bot)
 
     # Generate SSL context
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     # context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
 
     # And finally start webserver
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT, ssl_context=context)
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)  #, ssl_context=context
