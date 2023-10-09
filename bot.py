a86811078e9996db97536c74c04875e7390b5c7d
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2023-10-09 12:10:59 +0300

    webhook test

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import logging
+import os
 import ssl
 from urllib.parse import quote
 from typing import Union, Tuple
@@ -24,14 +25,14 @@ from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_applicati
 # bind localhost only to prevent any external access
 WEB_SERVER_HOST = "0.0.0.0"
 # Port for incoming request from reverse proxy. Should be any available port
-WEB_SERVER_PORT = 8443
+WEB_SERVER_PORT = int(os.getenv('PORT'))
 
 # Path to webhook route, on which Telegram will send requests
 WEBHOOK_PATH = "/webhook"
 # Secret key to validate requests from Telegram (optional)
 # Base URL for webhook will be used to generate webhook URL for Telegram,
 # in this example it is used public address with TLS support
-BASE_WEBHOOK_URL = "https://beauty-bloom-salon-bot-e05fc31e51e7.herokuapp.com:8443"
+BASE_WEBHOOK_URL = "https://beauty-bloom-salon-bot-e05fc31e51e7.herokuapp.com"  # :8443
 
 
 
